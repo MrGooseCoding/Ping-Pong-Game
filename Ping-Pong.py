1285f620c34b44ad9bfd50abd0ebd0f1b730754b
Mr. Goose <101597559+MrGooseCoding@users.noreply.github.com>
2022-04-10 19:18:58 +0200

    Create Ping-Pong.py
    Add the Ping-Pong.py file.

---
+++ Ping-Pong.py
@@ -0,0 +1,95 @@
+import turtle
+import random
+from time import sleep
+
+class ball(turtle.Turtle):
+    def __init__(self) -> None:
+        super().__init__()
+        self.penup()
+        self.shape('circle')
+        self.goto(0, 0)
+        self.dx = 1
+        self.dy = 1
+        self.speed(0)
+        self.color('white') 
+
+class paddle(turtle.Turtle):
+    def __init__(self, side):
+        super().__init__()
+        self.state = None
+        self.penup()
+        self.shape('square')
+        self.goto(-360 if side == 'left' else 360, 0)
+        self.color('white')
+        self.shapesize(5, 1)
+        return
+
+    def up(self):
+        if self.state == 'Up':
+            y = self.ycor()
+            self.goto(self.xcor(), y+2)
+            pass
+
+    def down(self):
+        if self.state == 'Down':
+            y = self.ycor()
+            self.goto(self.xcor(), y-2)
+            pass
+
+    def change_state_up(self):
+        self.state = 'Up'
+
+    def change_state_down(self):
+        self.state = 'Down'
+
+    def change_state_none(self):
+        self.state = None
+
+wn = turtle.Screen()
+wn.title("Ping-Pong by @MrGooseCoding")
+wn.setup(height=600, width=800)
+wn.bgcolor('black')
+wn.tracer(0, 0)
+paddle_a = paddle('left')
+paddle_b = paddle('right')
+ball = ball()
+
+while True:
+    wn.listen()
+    wn.onkeypress(paddle_a.change_state_up, 'w')
+    wn.onkeyrelease(paddle_a.change_state_none, 'w')
+    wn.onkeypress(paddle_a.change_state_down, 's')
+    wn.onkeyrelease(paddle_a.change_state_none, 's')
+    paddle_a.up()
+    paddle_a.down()
+    wn.onkeypress(paddle_b.change_state_up, 'Up')
+    wn.onkeyrelease(paddle_b.change_state_none, 'Up')
+    wn.onkeypress(paddle_b.change_state_down, 'Down')
+    wn.onkeyrelease(paddle_b.change_state_none, 'Down')
+    paddle_b.up()
+    paddle_b.down()
+    ball.sety(ball.ycor() + ball.dy)
+    ball.setx(ball.xcor() + ball.dx)
+    if ball.ycor() > 290:
+        ball.dy *= -1
+
+    if ball.ycor() < -290:
+        ball.dy *= -1
+
+    if ball.xcor() < -390:
+        ball.goto(0, 0)
+        ball.dx *= -1
+
+    if ball.xcor() > 390:
+        ball.goto(0, 0)
+        ball.dx *= -1
+
+    if (ball.xcor() > 340 and ball.xcor() < 350 and(ball.ycor() < paddle_b.ycor() + 50 and ball.ycor() > paddle_b.ycor() -50)):
+        ball.setx(340)
+        ball.dx *= -1 if ball.ycor() > paddle_b.ycor() -10 and ball.ycor() < paddle_b.ycor() +10 else -1*random.randrange(5, 15)/10
+    
+    if (ball.xcor() < -340 and ball.xcor()>-350 and(ball.ycor() < paddle_a.ycor() + 50 and ball.ycor() > paddle_a.ycor() -50)):
+        ball.setx(-340)
+        ball.dx *= -1 if ball.ycor() > paddle_a.ycor() -10 and ball.ycor() < paddle_a.ycor() +10 else -1*random.randrange(5, 15)/10
+
+    wn.update()
